df9cc9fbd897b01c376ea8371117fdcccc35096a
elnanni <elnanni@gmail.com>
2024-09-03 10:52:21 -0600

    Update on component test for tooltip

--- cypress/component/Content.cy.tsx
+++ cypress/component/Content.cy.tsx
@@ -29,9 +29,9 @@ describe("<Content />", () => {
   it("check badge and tooltip", () => {
     cy.mount(<Content />);
     cy.get('[data-test-id="badge-new-icon"]').should('be.visible');
-    cy.get('[data-test-id="badge-new-tooltip"]').should('not.exist');
+    cy.get('[data-test-id="badge-new-tooltip-text"]').should('not.exist');
     cy.get('[data-test-id="badge-new-text"]').trigger('mouseover');
-    cy.get('[data-test-id="badge-new-tooltip"]').should('be.visible');
+    cy.get('[data-test-id="badge-new-tooltip-text"]').should('be.visible');
     cy.get('[data-test-id="badge-new-text"]').click();
   });
 });
